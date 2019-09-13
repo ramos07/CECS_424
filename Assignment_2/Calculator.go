93194ffcf41213dc9f1ad254dd57e6664caabead
ramos07 <ramosricardo96@gmail.com>
2019-09-12 17:31:04 -0700

    Update Calculator.go

--- Assignment_2/Calculator.go
+++ Assignment_2/Calculator.go
@@ -2,23 +2,21 @@ package main
 
 import (
   "fmt"
-  "strconv" //used to convert string into integer
-  "log" //used for conversion
+  "os"
+  "strconv"
+  "log"
+  "strings"
 )
 
 func main() {
 
-  fmt.Println("Enter an expression: ")
-  var input string
-  fmt.Scan(&input)
+  input := strings.Split(os.Args[1], " ")
 
-  inputSlice := []rune(input) //Converting the given string into a slice
 
-  int1 := string(inputSlice[0]) //Taking the first element in the slice
-  operand := string(inputSlice[1]) //Taking the second element in the slice
-  int2 := string(inputSlice[2]) //Taking the third element in the slice
+  int1 := input[0]
+  operand := input[1]
+  int2 := input[2]
 
-  //Converting the strings to integrs
   integer1, err := strconv.Atoi(int1)
   if(err != nil) {
     log.Fatal(err)
@@ -28,21 +26,19 @@ func main() {
     log.Fatal(err)
   }
 
-  /*If and else statements to determine what operand to
-  perform on the given intergers and then perform that operation*/
   var result int
   if(operand == "+"){
     result = integer1 + integer2
-    fmt.Println(result)
   }else if (operand == "-"){
     result = integer1 - integer2
-    fmt.Println(result)
   }else if(operand == "*"){
     result = integer1*integer2
-    fmt.Println(result)
   }else{
     result = integer1/integer2
-    fmt.Println(result)
   }
+
+  fmt.Print(os.Args[1])
+  fmt.Print(" = ")
+  fmt.Print(result)
 
 }
